81c688a4d5fb0c347fb6eaf6a7179fa08c7bb3df
viktorcessan <viktor@cessan.se>
2023-11-24 23:29:26 +0100

    Added possibility to calculate perimeter, and that the user can select to display area, perimeter, or both

--- area_calculator.py
+++ area_calculator.py
@@ -3,8 +3,27 @@ width = float(input("Enter the width of the rectangle: "))
 height = float(input("Enter the height of the rectangle: "))
 unit = input("Enter the unit (e.g., cm, inches): ")
 
+# Ask the user if they want to calculate area, perimeter, or both
+choice = input("Do you want to calculate the Area (A), Perimeter (P), or Both (B)? ")
+
 # Calculate the area
 area = width * height
 
-# Print the area with the unit
-print(f"The area of the rectangle is: {area} {unit}²")
+# Calculate the perimeter
+perimeter = 2 * (width + height)
+
+
+# Checks the value of the choice and print the perimeter or both area and perimeter if the user selects both
+if choice == 'P' or choice == 'B':
+    perimeter = 2 * (width + height)
+
+# Displays results based on the users choice
+if choice == 'A':
+    print(f"The area of the rectangle is: {area} {unit}²")
+elif choice == 'P':
+    print(f"The perimeter of the rectangle is: {perimeter} {unit}")
+else:
+    print(f"The area of the rectangle is: {area} {unit}²")
+    print(f"The perimeter of the rectangle is: {perimeter} {unit}")
+
+
