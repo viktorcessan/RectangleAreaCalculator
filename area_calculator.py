81aff701763501a52c40f251fd378c2d31a037b4
viktorcessan <viktor@cessan.se>
2023-11-24 23:32:01 +0100

    Added welcome message, and changed order of decision-making to first prompt user to decide what to measure and display.

--- area_calculator.py
+++ area_calculator.py
@@ -1,17 +1,17 @@
+print("Hello and welcome to Viktors first rectangle area calculator")
+# Ask the user if they want to calculate area, perimeter, or both
+choice = input("Do you want to calculate the Area (A), Perimeter (P), or Both (B)? ")
+
 # Get user input for width, height, and unit
 width = float(input("Enter the width of the rectangle: "))
 height = float(input("Enter the height of the rectangle: "))
 unit = input("Enter the unit (e.g., cm, inches): ")
-
-# Ask the user if they want to calculate area, perimeter, or both
-choice = input("Do you want to calculate the Area (A), Perimeter (P), or Both (B)? ")
 
 # Calculate the area
 area = width * height
 
 # Calculate the perimeter
 perimeter = 2 * (width + height)
-
 
 # Checks the value of the choice and print the perimeter or both area and perimeter if the user selects both
 if choice == 'P' or choice == 'B':
